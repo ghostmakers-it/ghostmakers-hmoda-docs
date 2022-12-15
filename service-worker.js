4b77addca7a22727156355beb1e76920c3f897d8
euberdeveloper <euberdeveloper@users.noreply.github.com>
2022-12-15 01:41:03 +0000

    Deploying to gh-pages from @ ghostmakers-it/ghostmakers-hmoda-docs@e008d7e3b2eb40acb5b9424e6987b751f6e480cd 🚀

--- service-worker.js
+++ service-worker.js
@@ -27,7 +27,7 @@ self.addEventListener('message', (event) => {
 self.__precacheManifest = [
   {
     "url": "404.html",
-    "revision": "720b928984553b5c55e44e5c6c0cf362"
+    "revision": "212fc0a2844e71dc0e1d1a0a3527d493"
   },
   {
     "url": "assets/css/0.styles.0dbace68.css",
